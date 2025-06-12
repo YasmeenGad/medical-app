297505b378bfc744644b6fff53524e510f11cec3
YasmeenGad <gadyasmeen335@gmail.com>
2025-06-12 17:44:42 +0300

    chore: create map view file and add it to routes

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:medical_app/features/home/presentation/views/homeView.dart';
+import 'package:medical_app/features/map/presentation/views/map_view.dart';
 
 import '../../features/auth/presentation/views/login.dart';
 import '../utils/screens/under_build_screen.dart';
@@ -8,6 +9,7 @@ import 'base_routes.dart';
 class AppRoutes {
   static const String login = '/';
   static const String home = "home";
+  static const String map = "map";
 
   static Route<void> onGenerateRoute(RouteSettings settings) {
     final args = settings.arguments;
@@ -19,6 +21,10 @@ class AppRoutes {
       case AppRoutes.home:
         return BaseRoute(
           page: const HomeView(),
+        );
+      case AppRoutes.map:
+        return BaseRoute(
+          page: const MapView(),
         );
       default:
         return BaseRoute(page: const PageUnderBuildScreen());
